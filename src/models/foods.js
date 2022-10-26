3054985ed5a7f8b775563bf447ed60c8d0b8e0f5
Beatriz Alves <89867185+beatrizalves20@users.noreply.github.com>
2022-10-26 14:07:24 -0300

    Add files via upload

---
+++ src/models/foods.js
@@ -0,0 +1,24 @@
+const foods = [
+    { 
+      id: 1,
+      name: 'Hambúrguer',
+      image: 'imgs/hamburguer.jpg',
+    },
+    {
+      id: 2,
+      name: 'Suco',
+      image: 'imgs/suco.jpg',
+    },
+    {
+      id: 3,
+      name: 'Milkshake',
+      image: 'imgs/milkshake.jpg',
+    },
+    {
+      id: 4,
+      name: 'Sanduiche',
+      image: 'imgs/sanduiche.jpg',
+    },
+  ];
+
+  export default foods;
